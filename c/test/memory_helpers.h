b47bef957fdb2842128ab53aefcadb771248acb3
Victor Ng <victor@crankycoder.com>
2025-06-19 12:06:20 -0400

    test(memory): add memory tracking helpers header for FlatBuffer memory debugging

--- c/test/memory_helpers.h
+++ c/test/memory_helpers.h
@@ -1,8 +1,8 @@
 #ifndef MEMORY_HELPERS_H
 #define MEMORY_HELPERS_H
 
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 // Memory tracking structure
 typedef struct {
@@ -16,19 +16,19 @@ typedef struct {
 // Memory tracking functions
 void memory_tracking_init(void);
 void memory_tracking_cleanup(void);
+void memory_tracking_report(void);
+bool memory_tracking_has_leaks(void);
+size_t memory_tracking_get_allocated_bytes(void);
+size_t memory_tracking_get_allocation_count(void);
+
+// Tracked allocation functions
 void* tracked_malloc(size_t size, const char* file, int line);
 void* tracked_calloc(size_t nmemb, size_t size, const char* file, int line);
 void* tracked_realloc(void* ptr, size_t size, const char* file, int line);
 void tracked_free(void* ptr, const char* file, int line);
 char* tracked_strdup(const char* s, const char* file, int line);
 
-// Memory tracking report
-void memory_tracking_report(void);
-bool memory_tracking_has_leaks(void);
-size_t memory_tracking_get_allocated_bytes(void);
-size_t memory_tracking_get_allocation_count(void);
-
-// Macros for automatic file/line tracking
+// Convenience macros for automatic file/line tracking
 #define TRACKED_MALLOC(size) tracked_malloc(size, __FILE__, __LINE__)
 #define TRACKED_CALLOC(nmemb, size) tracked_calloc(nmemb, size, __FILE__, __LINE__)
 #define TRACKED_REALLOC(ptr, size) tracked_realloc(ptr, size, __FILE__, __LINE__)
@@ -36,11 +36,12 @@ size_t memory_tracking_get_allocation_count(void);
 #define TRACKED_STRDUP(s) tracked_strdup(s, __FILE__, __LINE__)
 
 // Test isolation helpers
-typedef void (*test_function_t)(void);
-void run_isolated_test(const char* test_name, test_function_t test_func);
+typedef int (*test_function_t)(void);
+int run_test_isolated(test_function_t test_func, const char* test_name);
+int run_test_with_valgrind(test_function_t test_func, const char* test_name);
 
-// Valgrind integration helpers
-bool is_running_under_valgrind(void);
-void valgrind_assert_no_leaks(void);
+// Valgrind integration
+bool is_valgrind_available(void);
+int run_valgrind_test(const char* executable, const char* test_name);
 
 #endif // MEMORY_HELPERS_H
